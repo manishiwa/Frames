8d9c59ac62e6a4e6c23cfefce8796328ebd83668
Aghostraa <ahouraazarbin@gmail.com>
2024-05-05 15:40:52 +0200

    Changes Saved

---
+++ app/api/frame/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const searchParams = req.nextUrl.searchParams
+  const id:any = searchParams.get("id")
+  const idAsNumber = parseInt(id)
+
+  const nextId = idAsNumber + 1
+
+  if(idAsNumber === 13){
+      return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame 7</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmY24GiPcr2WSGhsRzWeREpVtsAkkvCY7e4ChYGpq2diVV/13.png" />
+    <meta property="fc:frame:button:1" content="Visit growthepie.xyz" />
+    <meta property="fc:frame:button:1:action" content="post_redirect" />
+    <meta property="fc:frame:button:2" content="Visit Single Chain view" />
+    <meta property="fc:frame:button:2:action" content="post_redirect" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+  </head></html>`);
+  } else {
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame ${id}</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmY24GiPcr2WSGhsRzWeREpVtsAkkvCY7e4ChYGpq2diVV/${id}.png" />
+    <meta property="fc:frame:button:1" content="Next Page" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+  </head></html>`);
+  }
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source: '/growthepie',
+        destination: 'https://www.growthepie.xyz/',
+        permanent: false
+      },
+      {
+        source: '/modesinglechain',
+        destination: 'https://www.growthepie.xyz/chains/mode',
+        permanent: false
+      }
+    ]
+  }
+};
 
 export default nextConfig;
